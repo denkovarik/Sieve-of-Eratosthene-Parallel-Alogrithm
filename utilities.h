778fcf419ffcc3cc1ab76b3418e41734fa698471
Dennis <dennis.kovarik@mines.sdsmt.edu>
2020-09-17 20:29:29 -0600

    Added documentation

--- utilities.h
+++ utilities.h
@@ -1,3 +1,13 @@
+/**************************************************************************//**
+* @file
+*
+* Author: Dennis Kovarik
+* Date: 9/19/2020
+* Class Assignment: CSC 510 Program 1
+*
+* @brief Header file for the utility functions.
+*
+******************************************************************************/
 #include <stdbool.h> 
 #include <stdio.h>
 #include <stdlib.h>
